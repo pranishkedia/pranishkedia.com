5dbecca0a53d2488a27ca2794af9082690fccaca
pranishkedia <91530450+pranishkedia@users.noreply.github.com>
2024-10-29 21:50:10 +0000

    changes.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         pranishkedia.com
         <div></div>
-        coming soon.
+        website coming soon.
       </header>
     </div>
   );
