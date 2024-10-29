325d5797b34466f62ac13f84bb8f3ae4a1f7ebc2
pranishkedia <91530450+pranishkedia@users.noreply.github.com>
2024-10-29 18:36:51 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        pranishkedia.com
+        pranishkedia.com test
         <div></div>
         coming soon.
       </header>
